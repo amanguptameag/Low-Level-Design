0c38453e608bf8f24836d4dad3218da318c644dd
Aman Gupta <kaman5624@gmail.com>
2024-05-25 23:43:08 +0530

    update

--- book_my_show/booking.py
+++ book_my_show/booking.py
@@ -10,7 +10,7 @@ from seat import Seat
 from enums import *
 
 class Booking:
-    def __init__(self, booking_id: str, booking_date: date, user: User, audi: Audi, show: Show, booking_status: BookingStatus, total_amount: float):
+    def __init__(self, booking_id: str, booking_date: date, user: User, audi: Audi, show: Show, booking_status: BookingStatus, total_amount: float, payment_id: int):
         self.booking_id = booking_id
         self.booking_date = booking_date
         self.user = user
@@ -18,5 +18,5 @@ class Booking:
         self.show = show
         self.booking_status = booking_status
         self.total_amount = total_amount
+        self.payment_id: int = payment_id
         self.seats: List[Seat] = []
-        self.payment_obj: Optional[Payment] = None
--- book_my_show/payment.py
+++ book_my_show/payment.py
@@ -3,7 +3,8 @@ from datetime import date
 from enums import *
 
 class Payment:
-    def __init__(self, amount: float, payment_date: date, transaction_id: int, payment_status: PaymentStatus):
+    def __init__(self, payment_id: int, amount: float, payment_date: date, transaction_id: int, payment_status: PaymentStatus):
+        self.payment_id = payment_id
         self.amount = amount
         self.payment_date = payment_date
         self.transaction_id = transaction_id
